f045a87b6ef2ac02ff05521732e059dc5f238085
mazelsarrag <mazelsarrag@gmail.com>
2015-10-10 14:27:47 -0400

    so i add in input and out put functionality along the parameters

---
+++ LazySusan.py
@@ -0,0 +1,12 @@
+!#/usr/bin/env python # this will it just to run with calling it through
+import RPi.GPIO as GPIO
+in = raw_input #  this will take the input in the end
+GPIO.setup(6, GPIO.in) #
+GPIO.input(6, in) # this will take the input and  it should allow it to execute
+# the main thing
+GPiO.setup(13,GPIO.out) # this will allow it to make its
+GPIO.output(13,print(in)) # this will take the input and out print it
+GPIO.setup(16,GPIO.ack) #
+GPIO.ackput(16,false)  #
+GPIO.Setup(26,GPIO.TransmitCLK) #
+GPIO.TransmitCLKput(26,false) #
