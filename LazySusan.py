8f9de3bb5536b26c77e14fa2e3a06ea5f805f466
stumpc <cstump@email.unc.edu>
2015-10-11 06:45:23 -0400

    data formatting is annoying sometimes

--- LazySusan.py
+++ LazySusan.py
@@ -180,13 +180,16 @@ def read_bit(gpio_id):
         GPIO.add_event_detect(Thandshakein, GPIO.BOTH, callback=read_bit)
 
 def send_bit(gpio_id):
+    sendBit = []
+
     if(len(outStream) == 0):
         GPIO.remove_event_detect(Thandshakeout)
         finish_message()
 
     #now the logic for sending the message via outputWire and Thandshakeout bit by bit
     print(len(outStream))
-    if(outStream.read(bool, 1)[0]):
+    sendBit = outputStream.read(bool, 1)
+    if(sendBit[0]):
         GPIO.output(outputwire, GPIO.HIGH)
     else:
         GPIO.output(outputwire, GPIO.LOW)
